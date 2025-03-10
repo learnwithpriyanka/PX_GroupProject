1a116fec427d2b8c855d22e353c09377f6259194
Brijeshpriya <priyankakumari80921@gmail.com>
2025-03-10 20:35:14 +0530

    add footer

--- frontend/src/components/About/AboutPage.jsx
+++ frontend/src/components/About/AboutPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './Hero';
 import Team from './Team';
+import Footer from '../../Footer';
 
 function AboutPage() {
     return ( 
         <div>
          <Hero/>
          <Team/>
+         <Footer/>
         </div>
      );
 }
--- frontend/src/components/Contact/ContactPage.jsx
+++ frontend/src/components/Contact/ContactPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ContactMe from './ContactMe';
+import Footer from '../../Footer';
 
 function ContactPage() {
     return (  
         <div>
             <ContactMe/>
+            <Footer/>
         </div>
     );
 }
