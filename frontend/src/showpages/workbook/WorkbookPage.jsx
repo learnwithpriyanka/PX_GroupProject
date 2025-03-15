b07f92b2dfcb5699ddfb60b0bcf768cba8ea94f9
Nikhil Gupta <nikhilguptaraxaullx@gmail.com>
2025-03-15 18:28:23 +0530

    workbook added

---
+++ frontend/src/showpages/workbook/WorkbookPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Year from './Year';
+import Footer from '../../Footer';
+function WorkbookPage() {
+    return (  
+        <>
+       
+        <Year/>
+        <Footer/>
+        
+       
+        </>
+    );
+}
+
+export default WorkbookPage;
