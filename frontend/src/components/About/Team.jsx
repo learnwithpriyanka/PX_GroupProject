655add1dd9d9996b69a265d6bbd71cd1c2970cb2
Brijeshpriya <priyankakumari80921@gmail.com>
2025-03-16 14:22:08 +0530

    about change

--- frontend/src/components/About/Team.jsx
+++ frontend/src/components/About/Team.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import "./About.css";
 
 function Team() {
-    return ( 
-        <div>
-            <h1>about team</h1>
-        </div>
-     );
+  return (
+    <div>
+      <div className="team-container">
+        <h1 className="h11">Our Team</h1>
+        <div className="team">
+          <div className="team-member">
+            <img src="media/image/navnit.jpg" alt="team-member"  className="navnit"/>
+            
+            </div>
+
+            <div className="team-member-info">
+            <h5>Navnit</h5>
+              <h5>John Doe</h5>
+              <p>CEO & Co-Founder</p>
+                
+            </div>
+         </div>
+
+
+
+
+
+
+
+         
+            
+
+          
+        
+      </div>
+    </div>
+  );
 }
 
 export default Team;
